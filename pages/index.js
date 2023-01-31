e0a93ceaa19fd0e66b17a5d3bf012ff356a737df
tubi55 <tubi55@nate.com>
2023-01-31 17:47:33 +0900

    Image컴포넌트로 이미지 최적화

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Header from '@/components/Header';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import pic1 from '@/public/pic1.jpg';
+import Image from 'next/image';
 
 export default function Home() {
 	const [Dept, setDept] = useState([]);
@@ -26,6 +28,9 @@ export default function Home() {
 
 			<main className='Main'>
 				<h1>DCODELAB Main</h1>
+				<div className='pic'>
+					<Image src={pic1} alt='pic1' priority fill quality={50} />
+				</div>
 				{Dept.map((member, idx) => {
 					return (
 						<article key={idx}>
